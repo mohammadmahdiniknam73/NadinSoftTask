35ca1a52e480b5efcaa83b5675cd8e172f421c11
misaghmn73 <114310058+misaghmn73@users.noreply.github.com>
2023-11-10 06:47:52 +0330

    Optimization of Configurations

--- NadinSoft.Infrastructure/Configurations/ProductConfiguration.cs
+++ NadinSoft.Infrastructure/Configurations/ProductConfiguration.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using NadinSoft.Domain.Products;
+using NadinSoft.Domain.Users;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,18 +14,22 @@ namespace NadinSoft.Infrastructure.Configurations
     {
         public void Configure(EntityTypeBuilder<Product> builder)
         {
+            builder.HasKey(x => x.Id);
             builder.Property(p => p.Id).HasConversion(
                 productId => productId.Value,
                 value => new ProductId(value));
 
             builder.Property(p => p.ProduceDate).IsRequired();
+            builder.HasIndex(p => p.ProduceDate).IsUnique();
 
             builder.Property(p => p.ManufactureEmail).IsRequired();
+            builder.HasIndex(p => p.ManufactureEmail).IsUnique();
 
             builder.Property(p => p.ManufacturePhone).IsRequired();
 
             builder.Property(p => p.IsAvailable).IsRequired();
 
+          builder.HasOne<User>().WithMany().HasForeignKey(u=>u.Id).IsRequired;
         }
     }
 }
--- NadinSoft.Infrastructure/Configurations/UserConfiguration.cs
+++ NadinSoft.Infrastructure/Configurations/UserConfiguration.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using NadinSoft.Domain.Products;
 using NadinSoft.Domain.Users;
 using System;
 using System.Collections.Generic;
@@ -13,14 +14,17 @@ namespace NadinSoft.Infrastructure.Configurations
     {
         public void Configure(EntityTypeBuilder<User> builder)
         {
+            builder.HasKey(x => x.Id);
             builder.Property(u=>u.Id).HasConversion(
                 userId => userId.Value,
                 value => new UserId(value)).IsRequired();
 
-            builder.Property(u=>u.UserName).IsRequired();
-            builder.HasAlternateKey(u => u.UserName);
+            builder.Property(u=>u.UserName).HasMaxLength(50);
+            builder.HasIndex(u => u.UserName).IsUnique();
 
-            builder.Property(u=>u.Password).IsRequired();
+            builder.Property(u=>u.Password).HasMaxLength(16).IsRequired();
+
+            builder.HasMany<Product>().WithOne().IsRequired();
         }
     }
 }
